623ab1c88b2521d98a462ebb0744943c8f0ff160
Ryu <ryu@realtime.tw>
2023-12-17 22:25:27 +0800

    init project with counter

---
+++ src/content/index.ts
@@ -0,0 +1,29 @@
+import { runtime } from "webextension-polyfill";
+
+console.log("[content] loaded ");
+
+// add a naive counter
+let count = 0;
+
+type Listener = (event: MouseEvent) => void;
+
+function registerClickListener(listener: Listener) {
+  window.addEventListener("click", listener);
+}
+
+function countClicks() {
+  count++;
+  console.log("click(): ", count);
+
+  return runtime.sendMessage({
+    from: "content",
+    to: "background",
+    action: "click",
+  });
+}
+
+export function init() {
+  registerClickListener(countClicks);
+}
+
+init();
